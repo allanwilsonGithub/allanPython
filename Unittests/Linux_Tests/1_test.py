43dc374ff6d3177591860074cf0484e72cc6542e
Allan Wilson <allan@allanwilson.net>
2016-05-28 01:19:02 +0300

    added simple py script to run df -h on linux

---
+++ Unittests/Linux_Tests/1_test.py
@@ -0,0 +1,10 @@
+#usr/bin/env python
+
+import unittest
+import subprocess
+
+class Test_Linux_Setup(unittest.TestCase):
+    #TODO check cpu,mem,diskspace
+
+if __name__ == '__main__':
+    unittest.main()
