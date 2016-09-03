ffe49f906728c197dcba3408aef5927d618e143f
Allan Wilson <allan@allanwilson.net>
2016-09-03 19:16:59 +0300

    Added pytest examples

--- allanPython/FinnishFlashcards/finnishFlashcards.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-flashcard_dict ={}
-separator ='++separator++'
-
-
-inputFile = open('finnishFlashcardsInput.txt','r')
-for line in inputFile.readlines():
-    print line.split(separator)[0]
--- allanPython/Unittests/Windows_Tests/README.md
+++
@@ -1,3 +0,0 @@
-Requirements:
-
-pip install wmi
