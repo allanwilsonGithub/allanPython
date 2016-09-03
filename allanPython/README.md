df75549d98310c11e05cfb3c8ccc649457640055
Allan Wilson <allan@allanwilson.net>
2016-09-03 19:12:28 +0300

    .

---
+++ allanPython/README.md
@@ -0,0 +1,3 @@
+# allanPython
+This is a store for my scripts and projects while learning and improving my coding in Python
+
---
+++ allanPython/Unittests/Windows_Tests/1_test.py
@@ -0,0 +1,24 @@
+#usr/bin/env python
+
+import unittest
+
+class Test_Windows_Setup(unittest.TestCase):
+    def test_hard_disk_space(self):
+        import wmi
+
+        c = wmi.WMI ()
+        for d in c.Win32_LogicalDisk():
+            if d.DriveType == 3:
+                drive_type = "Local Disk"
+            elif d.DriveType == 2:
+                drive_type = "USB"
+            elif d.DriveType == 5:
+                drive_type = "CD Rom"
+            else:
+                drive_type = "Unexpected Drive Type"
+            print ("Drive Letter: %s, Diskspace %s %s, Drive Type: %s" %(d.Caption, d.FreeSpace, d.Size, drive_type))
+            self.assertIsNotNone(d)
+
+
+if __name__ == '__main__':
+    unittest.main()
