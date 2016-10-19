87e464a3f8212ac6b0cce7b735fc9ad3ce49d5d8
Allan Wilson <allan@allanwilson.net>
2016-10-19 23:17:48 +0300

    improved file_searcher

--- FileSearcher/txt_file_searcher.py
+++ FileSearcher/txt_file_searcher.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 
-#Variables:
-file_to_search = "D:\\DATA\\git\\allanPython\\FileSearcher\\leave_the_bourbon_on_the_shelf.txt"
-search_string = "satisfied"
-strings_found = []
+import os
+import sys
 
-#Open the file
-f = open(file_to_search, 'r')
-lines = f.readlines()
+files = ["leave_the_bourbon_on_the_shelf.txt",
+         "gotton.txt"]
+strings = ["satisfied",
+           "face",
+           "the"]
 
-#Search the file for each string
-for line in lines:
-    if search_string in line:
-        strings_found.append(line)
+def get_file(file_to_search):
+    try:
+        assert os.path.isfile(file_to_search)
+    except:
+        print("File does not exist.")
+        sys.exit(1)
+    f = open(file_to_search, 'r')
+    lines = f.readlines()
+    return lines
 
-#Display the results
-for result in strings_found:
-    print result
+def search_for_string(search_string,lines):
+    strings_found = []
+    for line in lines:
+        if search_string in line:
+            strings_found.append(line)
+    return strings_found
+
+def display_result(strings_found):
+    count = 0
+    for result in strings_found:
+        count += 1
+        print (str(count) + ": " + result.strip())
+
+    print("Total hits for \"" + string +"\": " + str(count))
+
+for file in files:
+    print("===== " + file + " =====")
+    lines = get_file(file)
+    for string in strings:
+        strings_found = search_for_string(string,lines)
+        display_result(strings_found)
